971f92d3cc82369f2f800c304de409083b8334dc
Denner Augusto De Paula Bernardes <41222616+denner-august@users.noreply.github.com>
2022-05-11 19:08:15 -0300

    Update results.test.ts

--- url-short/test/results.test.ts
+++ url-short/test/results.test.ts
@@ -1,16 +1,19 @@
 import axios from "axios";
 
 describe("testando chamadas a api", () => {
-  test("testando se ao passar string incorreta retorna erro", async () => {
-    const chamada = await axios.post(`http://localhost:3000/api/api`, {
+  const api = process.env.PRODUCTION
+    ? process.env.PRODUCTION
+    : "http://localhost:3000/api/api";
+  test("passar string incorreta deve retorna erro", async () => {
+    const chamada = await axios.post(`${api}`, {
       url: "google.com.br/",
     });
 
     expect(`${chamada.data}`).toMatch("Url inválida");
   });
 
-  test("testando o retorono da api ao passar string correta", async () => {
-    const chamada = await axios.post(`http://localhost:3000/api/api`, {
+  test("testando o retorno da api ao passar string correta", async () => {
+    const chamada = await axios.post(`${api}`, {
       url: "https://www.google.com.br/",
     });
 
